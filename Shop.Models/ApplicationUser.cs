c3ebd6d78c5bcd161fbf157273c031968ce6588d
ChienNguyen653653 <chiennguyen292002@gmail.com>
2023-11-20 15:19:27 +0700

    DEmo01

--- Shop.Models/ApplicationUser.cs
+++ Shop.Models/ApplicationUser.cs
@@ -24,6 +24,7 @@ namespace Shop.Models
         public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
-        public Company Company { get; set; }
+        public Company? Company { get; set; }
+        [NotMapped] public string Role { get; set; }
     }
 }
